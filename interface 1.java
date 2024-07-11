d3296c05a594ebf77fdb4894bd07bc1417cfbde3
vignesh-73772121219 <153163988+vignesh-73772121219@users.noreply.github.com>
2024-07-11 12:25:02 +0530

    Update interface 1.java

--- interface 1.java
+++ interface 1.java
@@ -1,20 +1,24 @@
-
-interface resize{
-	public void height(int h);
-	public void width(int w);
+import java.util.*;
+interface resizable{
+    public void width(int w);
+    public void height(int h);
 }
-public class jackrose implements resize{
-	public void height(int h) {
-		h=8;
-		System.out.println(h);
-	}
-	public void width(int w) {
-		w=7;
-		System.out.println(w);
-	}
-	public static void main(String[] args) {
-		jackrose ob= new jackrose();
-		ob.height(6);
-		ob.width(4);
-	}
+public  class rectangle implements resizable{
+    public void width(int w){
+        int width=88;
+        System.out.println("Default:"+width);
+        width=w;
+        System.out.println("Resized :"+width);
+    }
+    public void height(int h){
+       int width=888;
+        System.out.println("Default:"+width);
+        width=h;
+        System.out.println("Resized :"+width);
+    }
+    public static void main(String a[]){
+        rectangle r = new rectangle();
+        r.width(5);
+        r.height(4);
+    }
 }

79ae05ceeca7df66d63f51b6631a1322f61c8a39
vignesh-73772121219 <153163988+vignesh-73772121219@users.noreply.github.com>
2024-07-15 11:49:44 +0530

    Create Wrapperclass.java

---
+++ Wrapperclass.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class wrapper {
+	public static void main(String[] args) {	
+		 Integer m = new Integer(5);
+		 Character c =new Character('a');
+		 Float f=new Float(2.5);
+		 Double d=new Double(7.321);
+		 Long b = new Long(1234567);
+		 Boolean n = new Boolean(true);
+		 
+		 
+		 System.out.println("Integer :"+m);
+		 System.out.println("Character :"+c);
+		 System.out.println("Float :" +f);
+		 System.out.println("Double :"+d);
+		 System.out.println("Long :"+b);
+		 System.out.println("Boolean :"+n);
+
+	}
+
+}

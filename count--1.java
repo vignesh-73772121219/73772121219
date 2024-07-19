48394f14fc9f249b0c2df980e23aa32dcf8c0d6c
vignesh-73772121219 <153163988+vignesh-73772121219@users.noreply.github.com>
2024-07-19 13:40:06 +0530

    Update count--1.java

--- count--1.java
+++ count--1.java
@@ -1,13 +1,15 @@
 package calculator;
+
 import java.util.Scanner;
+
 public class Countn {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String st = sc.next();
-		int up = 0,lp =0,v=0,s =0,c=0,n=0;
+		String st = sc.nextLine();
+		int up = 0,lp =0,v=0,s =0,c=0,n=0 ,sp=0;
 				
 		char arr[] = st.toCharArray();
-    
 		for(int i=0;i<arr.length;i++) {
 		 if(arr[i]=='a' || arr[i]=='e' || arr[i]=='i' || arr[i]=='o' || arr[i]=='u' || arr[i]=='A' || arr[i]=='E' || arr[i]=='I' || arr[i]=='O' || arr[i]=='U' ) {
 				v++;
@@ -16,7 +18,6 @@ public class Countn {
 				 c++;
 			 }
 		}
-    
 		for(int i=0;i<arr.length;i++) {
 			if(Character.isUpperCase(arr[i])) {
 				up++;
@@ -24,12 +25,19 @@ public class Countn {
 			}
 			else  if(Character.isLowerCase(arr[i])){
 				lp++;
-			}	
+			}
+				
+			
+			
 			else if(arr[i]>=33 && arr[i]<= 47 || arr[i] >= 58 && arr[i]<= 64) {
 				s++;
 			}
-			else{
+			
+			else if( Character.isDigit(arr[i])){
 				n++;
+			}
+			else {
+				sp++;
 			}
 		
 }
@@ -38,6 +46,7 @@ public class Countn {
 		System.out.println("VOWELS :"+v);
 		System.out.println("SPECIAL CHAR :"+s);
 		System.out.println("CONSTANT :"+c);
+		System.out.println("SPACE :"+sp);
 		System.out.println(" NUMBER :"+n);
 	}
 
